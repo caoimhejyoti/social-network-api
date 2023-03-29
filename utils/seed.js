8063ba7676dfe8a17d78f5d19587ece27e1fd2e3
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-29 14:49:06 +0800

    prettier and commenting out reaction for now

--- utils/seed.js
+++ utils/seed.js
@@ -1,23 +1,23 @@
-const connection = require('../config/connection');
-const { User, Thought, Reaction } = require('../models');
-const { getRandomName, getRandomThoughts, getRandomReactions } = require('./data');
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { getRandomName, getRandomThoughts } = require("./data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
+connection.once("open", async () => {
+  console.log("connected");
   await User.deleteMany({});
   await Thought.deleteMany({});
-  await Reaction.deleteMany({});
+  // await Reaction.deleteMany({});
 
   const users = [];
   const thoughts = getRandomThoughts(10);
-  const reactions = getRandomReactions(10);
+  // const reactions = getRandomReactions(10);
 
   for (let i = 0; i < 20; i++) {
     const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
+    const first = fullName.split(" ")[0];
+    const last = fullName.split(" ")[1];
 
     users.push({
       first,
@@ -28,12 +28,11 @@ connection.once('open', async () => {
 
   await User.collection.insertMany(users);
   await Thought.collection.insertMany(thoughts);
-  await Reaction.collection.insertMany(reactions);
-
+  // await Reaction.collection.insertMany(reactions);
 
   console.table(users);
   console.table(thoughts);
-  console.table(reactions);
-  console.info('Seeding complete! 🌱');
+  // console.table(reactions);
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
