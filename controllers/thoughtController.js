e16b7094ec1ca8672b87de503f15f1629b77ce22
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-03 13:31:04 +0800

    create, update + delete thoughts coded. some bugs

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,13 +1,15 @@
 const Thought = require("../models/Thought");
-const User = require("./../models/User");
+const reactionSchema = require("../models/Thought");
 
 module.exports = {
+  //WORKING! Get all Thoughts
   getAllThoughts(req, res) {
     Thought.find()
       .populate("username")
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json({ message: err.message }));
   },
+  //WORKING! Get a Single Thought
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .populate("username")
@@ -18,9 +20,42 @@ module.exports = {
       )
       .catch((err) => res.status(500).json({ message: err.message }));
   },
-  //   createThought(req, res) {
-  //     Thought.create(req.body)
-  //       .then((dbthoughtData) => res.json(dbthoughtData))
-  //       .catch((err) => res.status(500).json({ message: err.message }));
-  //   },
+  // FIXME: Create a Thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbthoughtData) => res.json(dbthoughtData))
+      .catch((err) => res.status(500).json({ message: err.message }));
+  },
+  //FIXME: Update a Thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .populate("user")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //FIXME: Delete a Thought
+  deleteThought(req, res) {
+    // need to look at how to delete connected thoughts.
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      // trying to get bonus - FIXME: when used, causing a timeout error.
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : reactionSchema.deleteMany({ _id: { $in: thought.reactions } })
+      )
+      .then(() =>
+        res.status({
+          message: "thought with this id and its reactions are deleted",
+        })
+      )
+      .catch((err) => res.status(500).json({ message: err.message }));
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,23 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const {
   getAllThoughts,
   getSingleThought,
-  // createThought,
-  // updateThought,
-  // deleteThought,
+  createThought,
+  updateThought,
+  deleteThought,
   // createRaction,
   // deleteRaction,
-} = require('./../../controllers/thoughtController');
+} = require("./../../controllers/thoughtController");
 
 // /api/thoughts
-router.route('/').get(getAllThoughts)//.post(createThought);
+router.route("/").get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought)//.put(updateThought)//.delete(deleteThought);
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 // router.route('/:thoughtId/friends/:friendId').post(createRaction).delete(deleteRaction);
 
 module.exports = router;
-
